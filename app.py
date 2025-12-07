fc704dfc50a43abfe9a16e6af4175264460b7059
Evangeline Gifty <evangelinegiftyy@gmail.com>
2025-12-07 10:24:26 +0530

    Webhook 3

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+from flask import Flask, request
 
-from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello from Jenkins Docker build!"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    print("Webhook received:", data)  # You’ll see this in console
+    return "Webhook received!", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
